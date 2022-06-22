b559c741f326eabd806809014a4cacbf1fb0b1b8
rafiki777 <raftob@interia.pl>
2022-06-22 12:07:29 +0200

    Change file_structure's os.path to pathlib

--- files_structure.py
+++ files_structure.py
@@ -1,19 +1,16 @@
 import glob
-import os
+from pathlib import Path
 
 
 def get_files_structure(main_dir, extension):
-    if not (isinstance(main_dir, str) and os.path.isdir(main_dir)):
+    if not (isinstance(main_dir, str) and Path(main_dir).is_dir()):
         raise Exception("Please specify main_dir in config file")
+    data_dir = Path(main_dir)
     files_structure = {}
-    all_filepaths = sorted(
-        glob.glob(os.path.join(main_dir + f"/**/*{extension}"), recursive=True)
-    )
-    data_abs_dirs = set(os.path.dirname(filepath) for filepath in all_filepaths)
-    data_rel_dirs = [os.path.relpath(dir, main_dir) for dir in data_abs_dirs]
-    dirs = sorted(data_rel_dirs)
-    for directory in dirs:
-        filepaths = glob.glob(os.path.join(main_dir, directory) + f"/*{extension}")
-        filenames = [os.path.basename(filepath) for filepath in filepaths]
-        files_structure[directory] = sorted(filenames)
+    all_filepaths = sorted(data_dir.rglob(f"*{extension}"))
+    data_rel_dirs = sorted(set(filepath.parent.relative_to(data_dir) for filepath in all_filepaths))
+    for directory in data_rel_dirs:
+        filepaths = (main_dir / directory).glob(f"*{extension}")
+        filenames = [filepath.name for filepath in filepaths]
+        files_structure[str(directory)] = sorted(filenames)
     return files_structure
